6c5152d05c6aa0f2decfed62a193182b780e5e2f
Siravit Phokeed <siravitsasa@gmail.com>
2026-06-23 00:10:58 +0700

    refactor(demo): clean up Interactive Section

--- apps/demo/app/components/containers/components/InteractiveSection.tsx
+++ apps/demo/app/components/containers/components/InteractiveSection.tsx
@@ -11,15 +11,11 @@ const InteractiveSection: FC = () => (
     <Header>Interactive</Header>
     <Interactive
       shadowEffect
-      className="bg-secondary-container text-on-secondary-container state-layer-on-secondary-container flex h-14 w-24 flex-col rounded-sm px-4 py-2 text-left transition-[border,background-color,color] *:w-full *:truncate *:break-all"
+      className="bg-secondary-container text-on-secondary-container state-layer-on-secondary-container flex h-14 w-24 flex-col rounded-sm px-4 py-2 text-start transition-[border,background-color,color]"
       element="button"
     >
-      <Text type="title-medium" className="bg-transparent">
-        English
-      </Text>
-      <Text type="body-small" className="bg-transparent">
-        Atipol
-      </Text>
+      <Text type="title-medium">English</Text>
+      <Text type="body-small">Atipol</Text>
     </Interactive>
   </Section>
 );
