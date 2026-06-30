1eba7f83b069c1ebd1e21c9b638dcfd6e29afefd
Siravit Phokeed <siravitsasa@gmail.com>
2026-07-01 02:13:09 +0700

    fix(demo): Interactive demo missing background color

--- apps/demo/app/components/containers/components/InteractiveSection.tsx
+++ apps/demo/app/components/containers/components/InteractiveSection.tsx
@@ -11,7 +11,7 @@ const InteractiveSection: FC = () => (
     <Header>Interactive</Header>
     <Interactive
       shadowEffect
-      className="bg-secondary-container text-on-secondary-container state-layer-on-secondary-container grid! h-14 w-24 rounded-sm px-4 py-2 text-start transition-[border,background-color,color]"
+      className="bg-secondary-container! text-on-secondary-container state-layer-on-secondary-container grid! h-14 w-24 rounded-sm px-4 py-2 text-start transition-[border,background-color,color]"
       element="button"
     >
       <Text type="title-medium">English</Text>
