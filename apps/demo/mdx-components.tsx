03953124f16aea9a4eb66cc436831c2f660e8517
Siravit Phokeed <siravitsasa@gmail.com>
2026-07-04 23:07:31 +0700

    fix(docs): code blends in with blockquote

--- apps/demo/mdx-components.tsx
+++ apps/demo/mdx-components.tsx
@@ -19,7 +19,7 @@ const components: MDXComponents = {
     <Card
       appearance="filled"
       element="blockquote"
-      className="font-display my-4 px-4 py-1 [&_strong]:text-primary"
+      className="font-display [&_code]:bg-surface-bright [&_strong]:text-primary my-4 px-4 py-1"
     >
       <Text type="title-medium" {...props} />
     </Card>
