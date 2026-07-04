8d4fdebdc26b77d2fa8db15d51d3b6825958b9d8
Siravit Phokeed <siravitsasa@gmail.com>
2026-07-04 23:58:31 +0700

    feat(demo): fill out Docs Nav

--- apps/demo/app/docs/components/DocsNav.tsx
+++ apps/demo/app/docs/components/DocsNav.tsx
@@ -4,9 +4,7 @@ import cn from "@/lib/helpers/cn";
 import type { StyleableFC } from "@/lib/types";
 import { Card, CardHeader } from "@suankularb-components/react";
 
-const DocsNav: StyleableFC<{
-  open?: boolean;
-}> = ({ open, className, style }) => (
+const DocsNav: StyleableFC = ({ className, style }) => (
   <nav
     aria-label="Documentation"
     className={cn("space-y-2", className)}
@@ -16,10 +14,7 @@ const DocsNav: StyleableFC<{
       <CardHeader title="@suankularb-components/react" subtitle="v4.0.0" />
     </Card>
 
-    <DocsNavSection
-      title="Guides"
-      open // First section is always open
-    >
+    <DocsNavSection title="Guides" open>
       <DocsNavLink href="/docs/guides/getting-started">
         Getting started
       </DocsNavLink>
@@ -29,12 +24,30 @@ const DocsNav: StyleableFC<{
       <DocsNavLink href="/docs/guides/theming">Theming</DocsNavLink>
     </DocsNavSection>
 
-    <DocsNavSection title="Migrations" open={open}>
+    <DocsNavSection title="Migrations">
       <DocsNavLink href="/docs/migrations/v4">Migrating to v4</DocsNavLink>
       <DocsNavLink href="/docs/migrations/v3.3">Migrating to v3.3</DocsNavLink>
     </DocsNavSection>
 
-    <DocsNavSection title="Inputs" open={open}>
+    <DocsNavSection title="Layout & navigation">
+      <DocsNavLink href="/docs/layout/root-layout">Root Layout</DocsNavLink>
+      <DocsNavLink href="/docs/layout/content-layout">
+        Content Layout
+      </DocsNavLink>
+      <DocsNavLink href="/docs/layout/split-layout">Split Layout</DocsNavLink>
+      <DocsNavLink href="/docs/layout/columns">Columns</DocsNavLink>
+      <DocsNavLink href="/docs/layout/section">Section</DocsNavLink>
+      <DocsNavLink href="/docs/layout/nav-bar">Nav Bar</DocsNavLink>
+      <DocsNavLink href="/docs/layout/nav-drawer">Nav Drawer</DocsNavLink>
+      <DocsNavLink href="/docs/layout/app-drawer">App Drawer</DocsNavLink>
+      <DocsNavLink href="/docs/layout/tabs">Tabs</DocsNavLink>
+      <DocsNavLink href="/docs/layout/theme-provider">
+        Theme Provider
+      </DocsNavLink>
+      <DocsNavLink href="/docs/layout/anchor">Anchor</DocsNavLink>
+    </DocsNavSection>
+
+    <DocsNavSection title="Inputs">
       <DocsNavLink href="/docs/inputs/button">Button</DocsNavLink>
       <DocsNavLink href="/docs/inputs/segmented-button">
         Segmented Button
@@ -51,6 +64,38 @@ const DocsNav: StyleableFC<{
       <DocsNavLink href="/docs/inputs/search">Search</DocsNavLink>
       <DocsNavLink href="/docs/inputs/form-group">Form Group</DocsNavLink>
       <DocsNavLink href="/docs/inputs/form-item">Form Item</DocsNavLink>
+    </DocsNavSection>
+
+    <DocsNavSection title="Data display">
+      <DocsNavLink href="/docs/data/table">Table</DocsNavLink>
+      <DocsNavLink href="/docs/data/data-table">Data Table</DocsNavLink>
+      <DocsNavLink href="/docs/data/list">List</DocsNavLink>
+      <DocsNavLink href="/docs/data/card">Card</DocsNavLink>
+      <DocsNavLink href="/docs/data/assist-chip">Assist Chip</DocsNavLink>
+      <DocsNavLink href="/docs/data/filter-chip">Filter Chip</DocsNavLink>
+      <DocsNavLink href="/docs/data/input-chip">Input Chip</DocsNavLink>
+      <DocsNavLink href="/docs/data/suggestion-chip">
+        Suggestion Chip
+      </DocsNavLink>
+      <DocsNavLink href="/docs/data/chip-set">Chip Set</DocsNavLink>
+      <DocsNavLink href="/docs/data/text">Text</DocsNavLink>
+      <DocsNavLink href="/docs/data/header">Header</DocsNavLink>
+      <DocsNavLink href="/docs/data/page-header">Page Header</DocsNavLink>
+      <DocsNavLink href="/docs/data/avatar">Avatar</DocsNavLink>
+      <DocsNavLink href="/docs/data/material-icon">Material Icon</DocsNavLink>
+      <DocsNavLink href="/docs/data/divider">Divider</DocsNavLink>
+      <DocsNavLink href="/docs/data/interactive">Interactive</DocsNavLink>
+    </DocsNavSection>
+
+    <DocsNavSection title="Overlays">
+      <DocsNavLink href="/docs/overlays/dialog">Dialog</DocsNavLink>
+      <DocsNavLink href="/docs/overlays/full-screen-dialog">
+        Full-screen Dialog
+      </DocsNavLink>
+      <DocsNavLink href="/docs/overlays/menu">Menu</DocsNavLink>
+      <DocsNavLink href="/docs/overlays/menu-item">Menu Item</DocsNavLink>
+      <DocsNavLink href="/docs/overlays/snackbar">Snackbar</DocsNavLink>
+      <DocsNavLink href="/docs/overlays/progress">Progress</DocsNavLink>
     </DocsNavSection>
   </nav>
 );
