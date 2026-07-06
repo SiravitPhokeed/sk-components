97737ceac4d02572c88863ed74ed9d6577ab1d51
Siravit Phokeed <siravitsasa@gmail.com>
2026-07-06 16:41:39 +0700

    refactor(demo): prune styles in Interactive Section

--- apps/demo/app/components/data/components/InteractiveSection.tsx
+++ apps/demo/app/components/data/components/InteractiveSection.tsx
@@ -11,8 +11,8 @@ const InteractiveSection: FC = () => (
     <Header>Interactive</Header>
     <Interactive
       shadowEffect
-      className="bg-secondary-container text-on-secondary-container state-layer-on-secondary-container grid h-14 w-24 rounded-sm px-4 py-2 text-start transition-[border,background-color,color]"
       element="button"
+      className="bg-secondary-container text-on-secondary-container state-layer-on-secondary-container grid h-14 w-24 rounded-sm px-4 py-2 text-start"
     >
       <Text type="title-medium">English</Text>
       <Text type="body-small">Atipol</Text>
