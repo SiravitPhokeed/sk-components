d48dcea06f428421304e5de816e8d36f4cc1df53
Siravit Phokeed <siravitsasa@gmail.com>
2026-07-07 12:51:26 +0700

    feat(docs): add selectors for pages with multiple versions

---
+++ apps/demo/app/docs/components/RouterSelector.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button, SegmentedButton, Text } from "@suankularb-components/react";
+import Link from "next/link";
+import type { FC } from "react";
+
+const RouterSelector: FC<{ value: "app" | "pages" }> = ({ value }) => (
+  <section className="mt-4 space-y-2">
+    <Text type="title-small" element="h3" className="ms-2">
+      Next.js router
+    </Text>
+    <SegmentedButton alt="Router selection">
+      <Button
+        appearance="outlined"
+        selected={value === "app"}
+        href="/docs/guides/nextjs-app"
+        element={Link}
+      >
+        App Router
+      </Button>
+      <Button
+        appearance="outlined"
+        selected={value === "pages"}
+        href="/docs/guides/nextjs-pages"
+        element={Link}
+      >
+        Pages Router
+      </Button>
+    </SegmentedButton>
+  </section>
+);
+
+export default RouterSelector;
---
+++ apps/demo/app/docs/components/TailwindSelector.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button, SegmentedButton, Text } from "@suankularb-components/react";
+import Link from "next/link";
+import type { FC } from "react";
+
+const TailwindSelector: FC<{ value: "v3" | "v4" }> = ({ value }) => (
+  <section className="mt-4 space-y-2">
+    <Text type="title-small" element="h3" className="ms-2">
+      Tailwind CSS version
+    </Text>
+    <SegmentedButton alt="Tailwind CSS version selection">
+      <Button
+        appearance="outlined"
+        selected={value === "v4"}
+        href="/docs/guides/tailwindcss-v4"
+        element={Link}
+      >
+        v4
+      </Button>
+      <Button
+        appearance="outlined"
+        selected={value === "v3"}
+        href="/docs/guides/tailwindcss-v3"
+        element={Link}
+      >
+        v3
+      </Button>
+    </SegmentedButton>
+  </section>
+);
+
+export default TailwindSelector;
