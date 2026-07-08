94bcab19c51fe95384214171691adf2f3d9146f0
Siravit Phokeed <siravitsasa@gmail.com>
2026-07-09 01:16:18 +0700

    feat(docs): add modals and popovers guide

--- apps/demo/app/docs/components/DocsNav.tsx
+++ apps/demo/app/docs/components/DocsNav.tsx
@@ -30,6 +30,7 @@ const DocsNav: StyleableFC = ({ className, style }) => (
       <DocsNavLink href="/docs/guides/tailwindcss-v3">
         Tailwind CSS v3
       </DocsNavLink>
+      <DocsNavLink href="/docs/guides/modals">Modals and popovers</DocsNavLink>
       <DocsNavLink href="/docs/guides/localization">Localization</DocsNavLink>
       <DocsNavLink href="/docs/guides/theming">Theming</DocsNavLink>
     </DocsNavSection>
