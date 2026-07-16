0d67704a6bda57cbac6cbbf263ee399915961b47
Siravit Phokeed <26425747+SiravitPhokeed@users.noreply.github.com>
2026-07-17 06:00:42 +0700

    docs(demo): format BASE_URL JSDoc

--- apps/demo/lib/constants.ts
+++ apps/demo/lib/constants.ts
@@ -1,3 +1,5 @@
-/** The URL of the production deployment of this app, without a trailing
- * slash. */
+/**
+ * The URL of the production deployment of this app, without a trailing
+ * slash.
+ */
 export const BASE_URL = "https://sk-components-demo.mysk.school";
