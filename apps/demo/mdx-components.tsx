7b758d899ffdbfc46460f35c6900b2eead3ac4b5
Siravit Phokeed <26425747+SiravitPhokeed@users.noreply.github.com>
2026-07-22 00:03:17 +0700

    refactor(docs): use prose styles to render MDX

--- apps/demo/mdx-components.tsx
+++ apps/demo/mdx-components.tsx
@@ -22,15 +22,12 @@ const components: MDXComponents = {
       >
         {children}
         {isExternal && (
-          <>
-            <MaterialIcon
-              icon="arrow_outward"
-              size={20}
-              directional
-              alt="(opens in a new tab)"
-              className="-mt-1 inline-block"
-            />
-          </>
+          <MaterialIcon
+            icon="arrow_outward"
+            size={20}
+            directional
+            className="-mt-1 inline-block"
+          />
         )}
       </Element>
     );
@@ -42,30 +39,15 @@ const components: MDXComponents = {
     ]),
   ),
   blockquote: (props) => (
-    <Card
-      appearance="filled"
-      element="blockquote"
-      className="font-display [&_code]:bg-surface-bright [&_strong]:text-on-surface my-4 px-4 *:*:first:mt-3 *:*:last:mb-3 **:[blockquote,pre]:-mx-4 **:[blockquote,pre]:rounded-none"
-    >
+    <Card appearance="filled" element="blockquote" className="blockquote">
       <Text type="title-medium" {...props} />
     </Card>
-  ),
-  pre: ({ className, ...props }) => (
-    <Card
-      appearance="filled"
-      element={(props) => <pre {...props} dir="ltr" />}
-      className={cn(
-        "my-4 block overflow-x-auto text-base scheme-dark [&_code]:bg-transparent",
-        className,
-      )}
-      {...props}
-    />
   ),
   code: ({ "data-language": language, className, ...props }) => (
     <code
       className={cn(
         language
-          ? "w-fit p-4"
+          ? "py-3.5"
           : "bg-surface-variant rounded-xs px-1.5 text-[0.9em]",
         // Explicitly state font features for Firefox.
         "font-features-['ss01','ss02','ss03','ss04','ss05'] leading-normal tracking-normal",
@@ -75,16 +57,8 @@ const components: MDXComponents = {
     />
   ),
   hr: () => <Divider className="my-3" />,
-  ul: (props) => (
-    <ul className="marker:text-outline list-disc ps-6" {...props} />
-  ),
-  ol: (props) => (
-    <ol
-      className="marker:text-on-surface-variant marker:font-display list-decimal ps-6"
-      {...props}
-    />
-  ),
-  li: (props) => <li className="mb-1" {...props} />,
+  ul: (props) => <ul className="ul" {...props} />,
+  ol: (props) => <ol className="ol" {...props} />,
   p: (props) => <p className="my-2" {...props} />,
 };
 
