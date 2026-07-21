984551df4be5479b322b4b18609b702e1e71c19c
Siravit Phokeed <26425747+SiravitPhokeed@users.noreply.github.com>
2026-07-21 07:58:02 +0700

    fix(docs): turn Docs Nav Section into list

--- apps/demo/app/docs/components/DocsNavSection.tsx
+++ apps/demo/app/docs/components/DocsNavSection.tsx
@@ -24,9 +24,13 @@ const DocsNavSection: StyleableFC<{
         className="text-on-surface-variant group-open:rotate-90 group-open:rtl:-rotate-90"
       />
     </Interactive>
-    <div className="border-s-outline-variant ms-4 mt-1 border-s ps-2">
+    <ul
+      aria-label={title}
+      role="list"
+      className="border-s-outline-variant ms-4 mt-1 border-s ps-2"
+    >
       {children}
-    </div>
+    </ul>
   </details>
 );
 
